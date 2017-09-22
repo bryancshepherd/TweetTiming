6494f6c734da249d1123d7abb2163a2e0acb8e62
Bryan Shepherd <bryan.shepherd@windsorcircle.com>
2017-09-21 21:40:12 -0400

    Version 0.1

--- code/pull_data.py
+++ code/pull_data.py
@@ -4,10 +4,18 @@ import pandas as pd
 import datetime
 import dateutil
 import time
-### Better is to add this info
-### to a text file and pull it in
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 
 def calc_wait_time(rl_dict, access_point):
+    """
+    Calculate the amount of time to wait when we hit a rate limit
+    :param rl_dict: a dictionary of the current rate limit statuses
+    :param access_point: the API endpoint we're waiting on
+    :return: the seconds until the relevant wait limit will reset
+    """
+
     res_time = datetime.datetime.fromtimestamp(rl_dict[access_point + 'res'])
     current_time = datetime.datetime.now()
     seconds_until_reset = (res_time - current_time).seconds + 10
@@ -18,6 +26,11 @@ def calc_wait_time(rl_dict, access_point):
     return seconds_until_reset
 
 def rate_limit_check():
+    """
+    Check and store rate limit statuses
+    :return: a dictionary of the current rate limit statuses
+    """
+
     data = api.rate_limit_status()
 
     user_timeline_remaining = data['resources']['statuses'] \
@@ -58,14 +71,15 @@ def rate_limit_check():
             'vcres': verify_credentials_reset}
 
 def process_statuses(statuses):
-    '''
+    """
     Get information about each status into
     an easier to work with format
 
     :param statuses: A list of status objects from the
     Twitter API
     :return: A list of data elements
-    '''
+    """
+
     status_list = []
     for status in statuses:
 
@@ -83,15 +97,22 @@ def process_statuses(statuses):
 
     return pd.DataFrame(status_list)
 
-def get_users_tweets(user_obj, lookback_length, rlc):
-    '''
+def get_users_tweets(user_obj,
+                     lookback_length,
+                     max_tweets_per_day,
+                     rlc):
+    """
     Get a list of the relevant user's Twitter statues
 
     :param user_obj: a user object returned from the Twitter API
     :param lookback_period: how far to lookback in days
+    :param max_tweets_per_day: the maximum number of tweets to allow,
+    on average, per day. After a user passes this amount no more
+    statuses are pulled and their data is removed.
+    from the data.
     :return: a list of the users Twitter statuses for the duration of the
     lookback period
-    '''
+    """
 
     lookback_start = datetime.datetime.today() - \
                      dateutil.relativedelta.relativedelta(days=lookback_length)
@@ -99,11 +120,16 @@ def get_users_tweets(user_obj, lookback_length, rlc):
     single_user_tweets_df = pd.DataFrame()
     last_tweet_date = datetime.datetime.now()
     last_tweet_id = None
+    max_number_of_statuses = lookback_length * max_tweets_per_day
+    total_statused_pulled = 0
     # This is a little confusing, but if the
     # date of the last tweet pulled is more
     # recent than the beginning of the lookback
     # period, we want to keep pulling tweets
-    while last_tweet_date > lookback_start:
+    while ((last_tweet_date > lookback_start) &
+           (total_statused_pulled < max_number_of_statuses)):
+
+        print('Pulling tweets for {0}'.format(user_obj.screen_name))
 
         if rlc['utrem']<5:
             rlc = rate_limit_check()
@@ -116,6 +142,27 @@ def get_users_tweets(user_obj, lookback_length, rlc):
         rlc['utrem'] -= 1
 
         num_of_statuses = len(temp_statuses)
+
+        total_statused_pulled += num_of_statuses
+
+        # Get rid of data for users who tweet too much,
+        # they are probably bots.
+        if total_statused_pulled > max_number_of_statuses:
+            single_user_tweets_df = pd.DataFrame()
+            print('{0} exceeded the maximum number of '
+                  'tweets. Deleting.'.format(user_obj.screen_name))
+            break
+
+        # There is an issue where some followers
+        # end up with only one status that keeps repeating.
+        # This is a stopgap.
+        # To see it break (and maybe even fix it) get rid of this.
+        if num_of_statuses<=2:
+            print('Funky happened for {0}. '
+                  'Deleting.'.format(user_obj.screen_name))
+            single_user_tweets_df = pd.DataFrame()
+            break
+
         last_tweet_date = temp_statuses[num_of_statuses - 1].created_at
         last_tweet_id = temp_statuses[num_of_statuses - 1].id
 
@@ -127,15 +174,56 @@ def get_users_tweets(user_obj, lookback_length, rlc):
 
     return single_user_tweets_df, rlc
 
+def assign_numeric_day(x):
+    """
+    Assign a numeric value based on the day of the week. Useful for
+    creating series to sort by.
+    :param x:
+    :return: numeric value based on the date
+    """
+
+    if x == 'Sunday':
+        return 0
+    elif x == 'Monday':
+        return 1
+    elif x == 'Tuesday':
+        return 2
+    elif x == 'Wednesday':
+        return 3
+    elif x == 'Thursday':
+        return 4
+    elif x == 'Friday':
+        return 5
+    elif x == 'Saturday':
+        return 6
+
+def prep_for_plotting(dataframe, value_var):
+    """
+    Reshape the data for heatmap plotting
+    :param dataframe: the dataframe to be reshaped
+    :param value_var: the column that contains cell values
+    :return: reshaped data with weekdays as rows and hours as columns
+    """
+    reshaped_df = dataframe.pivot(index='weekday',
+                                  columns='hour',
+                                  values=value_var)
+    reshaped_df['day_name'] = reshaped_df.index.values
+    reshaped_df['numeric_day'] = reshaped_df.day_name.apply(
+        assign_numeric_day)
+    reshaped_df = reshaped_df.sort_values('numeric_day')
+    del reshaped_df['numeric_day']
+    del reshaped_df['day_name']
+
+    return reshaped_df
 
 # def get_followers(user_obj):
-#     '''
+#     """
 #     Get a list of the relevant user's Twitter statues
 #
 #     :param user_obj: a user object returned from the Twitter API
 #     :param lookback_period: how far to lookback in days
 #     :return: a list of the users Twitter followers
-#     '''
+#     """
 #
 #     single_user_tweets_list = []
 #     last_tweet_date = datetime.datetime.now()
@@ -167,31 +255,44 @@ def get_users_tweets(user_obj, lookback_length, rlc):
 #
 #     return single_user_tweets_list
 
+# Authentication details
+# There are a number of ways to do this.
 access_info = pd.read_csv('./access_info/access_info.txt')
+twitter_handle = access_info.get_values()[0,0]
+consumer_key = access_info.get_values()[0,1]
+consumer_secret = access_info.get_values()[0,2]
+access_token_key = access_info.get_values()[0,3]
+access_token_secret = access_info.get_values()[0,4]
 
-# Client details
-client_twitter_handle = access_info.get_values()[0,0]
-client_consumer_key = access_info.get_values()[0,1]
-client_consumer_secret = access_info.get_values()[0,2]
-client_access_token_key = access_info.get_values()[0,3]
-client_access_token_secret = access_info.get_values()[0,4]
-
-
-auth = tweepy.OAuthHandler(client_consumer_key, client_consumer_secret)
-auth.set_access_token(client_access_token_key, client_access_token_secret)
+# Make connection
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token_key, access_token_secret)
 api = tweepy.API(auth)
 
+# Get the authenticated user's information
 me = api.me()
-me.followers_count
 
+# Let's just put everything in a dataframe, so create an
+# empty dataframe to hold stuff.
 all_tweets_df = pd.DataFrame()
 rlc = rate_limit_check()
 
+# Get the list of the authenticated users followers
+# Right now max is 200, this will need to be
+# updated to handle more followers.
 followers = api.followers(count=me.followers_count)
 
+# Loop through the followers list to get individual
+# follower's timelines.
 for follower in followers:
 
-    data, rlc = get_users_tweets(follower, 14, rlc)
+    try:
+        data, rlc = get_users_tweets(follower, 28, 50, rlc)
+    except:
+        print('Fetching tweets for {0} '
+              'didn\'t work, skipping'.format(follower.screen_name))
+        continue
+
     all_tweets_df = pd.concat([all_tweets_df, data])
 
 
@@ -200,5 +301,60 @@ all_tweets_df.columns=['root_user', 'follower',
                        'tweet','is_retweet',
                        'tweet_time']
 
+# This is temporary, can be deleted later
+all_tweets_df_org = all_tweets_df
+# all_tweets_df = all_tweets_df_org
+all_tweets_df_org.to_pickle('./data/all_tweets_archive.pkl')
+all_tweets_df = pd.read_pickle('./data/all_tweets_archive.pkl')
+
 # It's easiest just to handle them here:
-# all_tweets_df = all_tweets_df.drop_duplicates
+all_tweets_df.drop_duplicates(inplace=True)
+
+all_tweets_df['weekday'] = all_tweets_df.tweet_time.dt.weekday_name
+all_tweets_df['hour'] = all_tweets_df.tweet_time.dt.hour
+
+all_tweets_df_by_hour = all_tweets_df\
+                            .groupby(['weekday', 'hour'])\
+                            .follower\
+                            .count().reset_index()
+
+# Two grouping/counting steps to
+# get to the number of users per hour
+users_by_hour = all_tweets_df\
+                 .groupby(['weekday', 'hour', 'follower'])\
+                 .tweet\
+                 .count().reset_index()
+
+users_by_hour = users_by_hour\
+                 .groupby(['weekday', 'hour'])\
+                 .follower.count()\
+                 .reset_index()
+
+# Two grouping/counting steps to
+# get to the number of retweets per hour
+retweets_by_hour = all_tweets_df\
+                     .groupby(['weekday', 'hour', 'follower'])\
+                     .is_retweet\
+                     .sum().reset_index()
+
+retweets_by_hour = retweets_by_hour\
+                     .groupby(['weekday', 'hour'])\
+                     .is_retweet\
+                     .sum().reset_index()
+
+sns.set()
+
+# Draw a heatmap with the numeric values in each cell
+df_to_plot = prep_for_plotting(all_tweets_df_by_hour, 'follower')
+f, ax = plt.subplots(figsize=(9, 6))
+sns.heatmap(df_to_plot, annot=True, fmt="d", linewidths=.5, ax=ax)
+
+df_to_plot = prep_for_plotting(all_tweets_df_by_hour, 'follower')
+ax = sns.heatmap(df_to_plot, linewidths=.5)
+
+df_to_plot = prep_for_plotting(users_by_hour, 'follower')
+ax = sns.heatmap(df_to_plot, linewidths=.5, square=True)
+
+df_to_plot = prep_for_plotting(retweets_by_hour, 'is_retweet')
+ax = sns.heatmap(df_to_plot, linewidths=.5, square=True)
+
